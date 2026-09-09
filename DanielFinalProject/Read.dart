c74758eac202c4b15733c94f2cc2ad9a2faa98b0
DanielNotMelter <daniel.melter20@gmail.com>
2026-09-09 16:58:50 +0300

    exercise 3 done

--- DanielFinalProject/Read.dart
+++ DanielFinalProject/Read.dart
@@ -1,5 +1,6 @@
 
 // TEST 123
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:io';
 class Read{
@@ -111,5 +112,42 @@ class Read{
         return engineWorkedWell;
         
     
+    }
+    jokeAPI(String url) async {
+
+        int longestJoke = 0;
+        int shortestJoke = 10000000;
+        double avregeJoke = 0;
+        int overallJokes = 0;
+        
+        for(int y = 0; y < 10; y++) {
+          var URL = Uri.parse(url);
+          var response = await http.get(URL);
+
+          Map<String, dynamic> ApiResponse = jsonDecode(response.body);
+          List<String> Keys = ApiResponse.keys.toList();  
+          List<dynamic> values = ApiResponse.values.toList();  
+          String joke;
+          joke = "ooo";
+
+          for(int b = 0; b < Keys.length; b++) {
+            if(Keys[b] == "joke") {
+              joke = values[b];
+              break;
+            }
+          }
+          print(joke);
+          if(joke.length > longestJoke) {
+            longestJoke = joke.length;
+          } else if(joke.length < shortestJoke) {
+            shortestJoke = joke.length;
+          }
+          overallJokes += joke.length;
+        }
+        avregeJoke = (overallJokes / 10);
+        print(avregeJoke);
+        print(longestJoke);
+        print(shortestJoke);
+ 
     }
 }
--- DanielFinalProject/main.dart
+++ DanielFinalProject/main.dart
@@ -1,5 +1,5 @@
 import 'Read.dart';
 void main(){
 Read read = new Read();
-    print(read.engineWork('DanielFinalProject/data.json'));
+    print(read.jokeAPI('https://v2.jokeapi.dev/joke/Any?type=single'));
 }
